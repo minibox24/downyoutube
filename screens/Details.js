fd6cb390cce2eeaa9eb042e8c0a7571465819e7a
minibox24 <minibox724@gmail.com>
2021-10-04 23:49:59 +0900

    details

--- screens/Details.js
+++ screens/Details.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, Image, ActivityIndicator, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 export default function DetailsScreen({ navigation, route }) {
   const [data, setData] = React.useState({
@@ -11,7 +18,7 @@ export default function DetailsScreen({ navigation, route }) {
   });
 
   React.useEffect(() => {
-    fetch(`https://dyapi.loca.lt/info?query=${route.params.query}`)
+    fetch(`http://127.0.0.1:8000/info?query=${route.params.query}`)
       .then((res) => res.json())
       .then((data) => {
         setData({
@@ -34,13 +41,28 @@ export default function DetailsScreen({ navigation, route }) {
 
   return (
     <View style={styles.main}>
-      <Text>{data.id}</Text>
-      <Text>{data.title}</Text>
-      <Text>{data.uploader}</Text>
       <Image
         source={{ uri: data.thumbnail }}
-        style={{ width: 305, height: 159 }}
+        style={{
+          width: 320,
+          height: 180,
+          resizeMode: "contain",
+        }}
       />
+      <View style={styles.info}>
+        <Text style={styles.title} numberOfLines={1}>
+          {data.title}
+        </Text>
+        <Text numberOfLines={1}>{data.uploader}</Text>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity onPress={() => {}} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>영상</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>음성</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -50,5 +72,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  info: {
+    width: 320,
+    marginTop: 15,
+  },
+  title: {
+    fontSize: 17,
+  },
+  buttons: {
+    marginTop: 30,
+    width: 320,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  buttonContainer: {
+    margin: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: "#747F8D",
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 15,
   },
 });
