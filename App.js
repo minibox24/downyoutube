2d94877e42594b2077d95fa0129db905210636c8
minibox24 <minibox724@gmail.com>
2021-10-04 21:07:57 +0900

    app, loadfont

--- App.js
+++ App.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from './screens/Home';
-import DetailsScreen from './screens/Details';
-
+import HomeScreen from "./screens/Home";
+import DetailsScreen from "./screens/Details";
 
 function App() {
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
+  const [isReady, setIsReady] = React.useState(false);
+
+  const LoadFonts = async () => {
+    await Font.loadAsync({
+      pretendard: require("./assets/fonts/Pretendard-Black.otf"),
+    });
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={HomeScreen}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default App;
